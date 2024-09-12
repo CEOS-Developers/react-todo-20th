a17251a4e0fada6b4f10e0f4f02707a4c3bc709f
HAE2NI <hae0_02ni@ewhain.net>
2024-09-12 16:06:11 +0900

    ✨ feat: zustand 함수 추가

--- week2-ceos/src/components/Header/Header.jsx
+++ week2-ceos/src/components/Header/Header.jsx
@@ -1,13 +1,26 @@
 import styled from "styled-components";
 import { rowFlex } from "@styles/commonStyle";
 import { headerDate } from "../../utils/data";
+import { useStore } from "zustand";
 
 export default function Header() {
-  return <HeaderWrapper>{headerDate}</HeaderWrapper>;
+  const newWeek = useStore((state) => state.increaseWeek);
+  const pastWeek = useStore((state) => state.decreaseWeek);
+
+  return (
+    <HeaderWrapper>
+      <OtherWeekButton onClick={pastWeek}>⬅️</OtherWeekButton>
+      {headerDate}
+      <OtherWeekButton onClick={newWeek}>➡️</OtherWeekButton>
+    </HeaderWrapper>
+  );
 }
 
 const HeaderWrapper = styled.header`
   ${rowFlex}
-
+  padding: 5rem;
   ${({ theme }) => theme.fonts.Headline1};
+  color: ${({ theme }) => theme.colors.black};
 `;
+
+const OtherWeekButton = styled.button``;
