7ce6cc0a235e87d98957573a6d3606732472eca4
HAE2NI <hae0_02ni@ewhain.net>
2024-09-12 19:28:47 +0900

    ✨ feat: Header 양 쪽 버튼 기능 추가

--- week2-ceos/src/components/Header/Header.jsx
+++ week2-ceos/src/components/Header/Header.jsx
@@ -1,17 +1,17 @@
 import styled from "styled-components";
 import { rowFlex } from "@styles/commonStyle";
-import { headerDate } from "../../utils/data";
-import { useStore } from "zustand";
+import { headerDate } from "@utils/data";
+import { useStore } from "@core/store.js";
 
 export default function Header() {
-  const newWeek = useStore((state) => state.increaseWeek);
-  const pastWeek = useStore((state) => state.decreaseWeek);
+  const upDays = useStore((state) => state.increaseWeek);
+  const downDays = useStore((state) => state.decreaseWeek);
 
   return (
     <HeaderWrapper>
-      <OtherWeekButton onClick={pastWeek}>⬅️</OtherWeekButton>
+      <OtherWeekButton onClick={downDays}>⬅️</OtherWeekButton>
       {headerDate}
-      <OtherWeekButton onClick={newWeek}>➡️</OtherWeekButton>
+      <OtherWeekButton onClick={upDays}>➡️</OtherWeekButton>
     </HeaderWrapper>
   );
 }
