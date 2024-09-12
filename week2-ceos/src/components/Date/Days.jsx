61979a9d63c442a1a425c5d5799e65b688201cc7
HAE2NI <hae0_02ni@ewhain.net>
2024-09-12 19:28:26 +0900

    ✨ feat: 날짜 zustand로 옮기기

--- week2-ceos/src/components/Date/Days.jsx
+++ week2-ceos/src/components/Date/Days.jsx
@@ -1,24 +1,22 @@
-import { addDays, endOfWeek, format, startOfWeek } from "date-fns";
-import { newDate } from "@utils/data";
+import { addDays, format } from "date-fns";
 import styled from "styled-components";
 import { rowFlex } from "@styles/commonStyle";
+import { useStore } from "../../core/store";
 
 //날짜들 부분
 export default function Days() {
-  let weekStart = startOfWeek(newDate);
-
   let days = [];
+  const thisweek = useStore((state) => state.weekStart);
 
+  // 현재 주의 날짜들을 생성
   for (let i = 0; i < 7; i++) {
-    const currentDay = addDays(weekStart, i);
-
+    const currentDay = addDays(thisweek, i);
     days.push(<DayItem key={i}>{format(currentDay, "d")}</DayItem>);
   }
-
-  return <Wrapper>{days}</Wrapper>;
+  return <WeekWrapper>{days}</WeekWrapper>;
 }
 
-const Wrapper = styled.div`
+const WeekWrapper = styled.div`
   ${rowFlex}
   justify-content: space-between;
   padding: 2rem 0;
