407d1ea04148770900eb001393b674b5633e3ba2
HAE2NI <hae0_02ni@ewhain.net>
2024-09-12 19:29:19 +0900

    🎨 style: SUIT-Regular로 수정

--- week2-ceos/src/styles/theme.js
+++ week2-ceos/src/styles/theme.js
@@ -13,56 +13,56 @@ const colors = {
 
 const fonts = {
   Headline1: css`
-    font-family: SUIT;
-    font-size: 4.8rem;
+    font-family: SUIT-Regular;
+    font-size: 3.2rem;
     font-weight: 600;
     line-height: 6rem;
   `,
 
   Body1: css`
-    font-family: SUIT;
+    font-family: SUIT-Regular;
     font-size: 3rem;
     font-weight: 600;
     line-height: 2.8rem;
   `,
   Body2: css`
-    font-family: SUIT;
+    font-family: SUIT-Regular;
     font-size: 3rem;
     font-weight: 500;
     line-height: 2.8rem;
   `,
   Body3: css`
-    font-family: SUIT;
+    font-family: SUIT-Regular;
     font-size: 1.6rem;
     font-weight: 600;
     line-height: 2.4rem;
   `,
   Body4: css`
-    font-family: SUIT;
+    font-family: SUIT-Regular;
     font-size: 1.6rem;
     font-weight: 400;
     line-height: 2.4rem;
   `,
   Body5: css`
-    font-family: SUIT;
+    font-family: SUIT-Regular;
     font-size: 1.4rem;
     font-weight: 600;
     line-height: 2rem;
   `,
   Body6: css`
-    font-family: SUIT;
+    font-family: SUIT-Regular;
     font-size: 1.4rem;
     font-weight: 500;
     line-height: 2rem;
   `,
   Body7: css`
-    font-family: SUIT;
+    font-family: SUIT-Regular;
     font-size: 1.2rem;
     font-weight: 600;
     line-height: 1.6rem;
   `,
   Body8: css`
-    font-family: SUIT;
+    font-family: SUIT-Regular;
     font-size: 1.2rem;
     font-weight: 500;
     line-height: 1.6rem;
