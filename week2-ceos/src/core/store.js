40d6bdf0fd12a3a9e825cc4ea7cb8694a6a7a176
HAE2NI <hae0_02ni@ewhain.net>
2024-09-12 19:15:27 +0900

    ✨ feat: useStore 보정

--- week2-ceos/src/core/store.js
+++ week2-ceos/src/core/store.js
@@ -1,10 +1,34 @@
 import { create } from "zustand";
-import { weekStart } from "../utils/data";
+import { weekStart } from "@utils/data";
+import { addDays, format, startOfDay, startOfWeek, subDays } from "date-fns";
 
-const useStore = create((set) => ({
-  startDay: weekStart,
-  increaseWeek: () => set((state) => ({ startDay: state.startDay + 7 })),
-  decreaseWeek: () => set((state) => ({ startDay: state.startDay - 7 })),
+const newDate = new Date();
+
+export const useStore = create((set) => ({
+  weekStart: startOfWeek(newDate),
+  startDay: format(weekStart, "d"),
+
+  increaseWeek: () =>
+    set((state) => {
+      const oneWeekLater = addDays(state.weekStart, 7);
+      return {
+        weekStart: oneWeekLater,
+        startDay: format(oneWeekLater, "d"),
+      };
+    }),
+
+  decreaseWeek: () =>
+    set((state) => {
+      const oneWeekAgo = subDays(state.weekStart, 7);
+      return {
+        weekStart: oneWeekAgo,
+        startDay: format(oneWeekAgo, "d"),
+      };
+    }),
 }));
 
-export { useStore };
+// export const useTestStore = create((set) => ({
+//   count: 0,
+//   increase: () => set((state) => ({ count: state.count + 1 })),
+//   decrease: () => set((state) => ({ count: state.count - 1 })),
+// }));
