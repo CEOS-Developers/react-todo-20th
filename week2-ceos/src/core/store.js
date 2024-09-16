23ab57d1c5e1cb6c04d4e7f0106dad7d767244ba
HAE2NI <hae0_02ni@ewhain.net>
2024-09-16 13:37:05 +0900

    ✨ feat: zustand store에 headerDay 추가

--- week2-ceos/src/core/store.js
+++ week2-ceos/src/core/store.js
@@ -1,12 +1,14 @@
 import { create } from "zustand";
 import { weekStart } from "@utils/data";
 import { addDays, format, startOfDay, startOfWeek, subDays } from "date-fns";
+import { headerDate } from "../utils/data";
 
 const newDate = new Date();
 
 export const useStore = create((set) => ({
   weekStart: startOfWeek(newDate),
   startDay: format(weekStart, "d"),
+  headerDay: `${format(weekStart, "yyyy")}년 ${format(weekStart, "MM")}월`,
 
   increaseWeek: () =>
     set((state) => {
@@ -14,6 +16,7 @@ export const useStore = create((set) => ({
       return {
         weekStart: oneWeekLater,
         startDay: format(oneWeekLater, "d"),
+        headerDay: `${format(oneWeekLater, "yyyy")}년 ${format(oneWeekLater, "MM")}월`,
       };
     }),
 
@@ -23,6 +26,7 @@ export const useStore = create((set) => ({
       return {
         weekStart: oneWeekAgo,
         startDay: format(oneWeekAgo, "d"),
+        headerDay: `${format(oneWeekAgo, "yyyy")}년 ${format(oneWeekAgo, "MM")}월`,
       };
     }),
 }));
