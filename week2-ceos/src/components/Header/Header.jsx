e5f877af5b0e8abd70248d5fba0606ce9c759d4e
HAE2NI <hae0_02ni@ewhain.net>
2024-09-16 13:37:21 +0900

    ✨ feat: 날짜 바꾸면 header날짜도 같이 바뀌도록 추가

--- week2-ceos/src/components/Header/Header.jsx
+++ week2-ceos/src/components/Header/Header.jsx
@@ -1,16 +1,16 @@
 import styled from "styled-components";
 import { rowFlex } from "@styles/commonStyle";
-import { headerDate } from "@utils/data";
 import { useStore } from "@core/store.js";
 
 export default function Header() {
   const upDays = useStore((state) => state.increaseWeek);
   const downDays = useStore((state) => state.decreaseWeek);
+  const headerText = useStore((state) => state.headerDay);
 
   return (
     <HeaderWrapper>
       <OtherWeekButton onClick={downDays}>⬅️</OtherWeekButton>
-      {headerDate}
+      {headerText}
       <OtherWeekButton onClick={upDays}>➡️</OtherWeekButton>
     </HeaderWrapper>
   );
