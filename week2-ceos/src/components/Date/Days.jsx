fbb20c0dc7356c347ff973deffc80c40f9d2315f
HAE2NI <hae0_02ni@ewhain.net>
2024-09-16 16:59:18 +0900

    ✨ feat: 클릭한 날짜로 이동

--- week2-ceos/src/components/Date/Days.jsx
+++ week2-ceos/src/components/Date/Days.jsx
@@ -3,23 +3,34 @@ import styled from "styled-components";
 import { rowFlex } from "@styles/commonStyle";
 import { useStore } from "@core/store";
 import DayItem from "./DayItem";
+import TodosHeader from "../Todos/TodosHeader";
+import { useState } from "react";
+import { ko } from "date-fns/locale";
 
 //날짜들 부분
 export default function Days() {
   let days = [];
-  let clickedDate;
+  const formattedtoday = format(new Date(), "MM월 dd일 EEEE", { locale: ko });
+  const [clickedDate, setClickedDate] = useState(formattedtoday);
   const thisweek = useStore((state) => state.weekStart);
 
   // 현재 주의 날짜들을 생성
   for (let i = 0; i < 7; i++) {
     const currentDay = addDays(thisweek, i);
+
     function handleClickDay() {
-      clickedDate = currentDay;
-      console.log("here", clickedDate);
+      const formattedClickedDate = format(currentDay, "MM월 dd일 EEEE", { locale: ko });
+      setClickedDate(formattedClickedDate);
     }
+
     days.push(<DayItem handleClickDay={handleClickDay} key={currentDay} currentDay={currentDay} />);
   }
-  return <WeekWrapper>{[...days]}</WeekWrapper>;
+  return (
+    <>
+      <WeekWrapper>{[...days]}</WeekWrapper>
+      <TodosHeader clickedDate={clickedDate} />
+    </>
+  );
 }
 
 const WeekWrapper = styled.div`
