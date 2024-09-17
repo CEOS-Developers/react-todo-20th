bd7a646fd88710d2b4a5b50e11b5382558707a2a
HAE2NI <hae0_02ni@ewhain.net>
2024-09-17 17:13:21 +0900

    💄 design: todoList 커스텀

--- week2-ceos/src/components/Todos/TodosList.jsx
+++ week2-ceos/src/components/Todos/TodosList.jsx
@@ -1,32 +1,34 @@
 import { useClickedDayStore } from "../../core/store";
 import styled from "styled-components";
-import { rowFlex } from "@styles/commonStyle";
+import { columnFlex } from "@styles/commonStyle";
 import OneTodo from "./OneTodo";
 import { useEffect, useState } from "react";
 
 export default function TodosList() {
   const choosedDate = useClickedDayStore((state) => state.clickedDay);
   const todosList = useClickedDayStore((state) => state.todoList);
+
   const [filterTodoList, setFilteredTodoList] = useState([]);
 
   useEffect(() => {
     const filtered = todosList.filter((list) => list.day === choosedDate);
     filtered.length > 0 && setFilteredTodoList(filtered[0].todos);
+    console.log(filterTodoList);
   }, [filterTodoList, todosList, choosedDate]);
 
   return (
     <Wrapper>
       {filterTodoList.length > 0 &&
         filterTodoList.map((content) => {
-          const { text, isDone } = content;
-          return <OneTodo text={text} isDone={isDone} key={text.length + Math.random()} />;
+          const { text, isDone, boxColor } = content;
+          return <OneTodo boxColor={boxColor} text={text} isDone={isDone} key={text.length + Math.random()} />;
         })}
     </Wrapper>
   );
 }
 
 const Wrapper = styled.ul`
-  ${rowFlex}
-  height: 100%;
+  ${columnFlex}
+  ${({ theme }) => theme.fonts.Body5};
   margin: 2rem;
 `;
