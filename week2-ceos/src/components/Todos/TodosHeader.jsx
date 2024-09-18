f12e4cae2559e38c93a0efa9e02769f6fc79c81d
HAE2NI <hae0_02ni@ewhain.net>
2024-09-18 22:28:31 +0900

    ✨ feat: 완료 체크

--- week2-ceos/src/components/Todos/TodosHeader.jsx
+++ week2-ceos/src/components/Todos/TodosHeader.jsx
@@ -1,14 +1,42 @@
 import styled from "styled-components";
 import PropTypes from "prop-types";
+import { useTodoStore } from "../../core/store";
+import { useEffect, useState } from "react";
 
 TodosHeader.propTypes = {
   clickedDate: PropTypes.string.isRequired,
 };
 
 export default function TodosHeader({ clickedDate }) {
+  const todosList = useTodoStore((state) => state.todoList);
+  const choosedDate = useTodoStore((state) => state.clickedDay);
+  const [filterTodoList, setFilteredTodoList] = useState([]);
+  const [isDoneList, setIsDoneList] = useState([]);
+
+  useEffect(() => {
+    const filtered = todosList.filter((list) => list.day === choosedDate);
+    if (filtered.length > 0) {
+      setFilteredTodoList(filtered[0].todos);
+    } else {
+      setFilteredTodoList([]);
+    }
+  }, [todosList, choosedDate]);
+
+  useEffect(() => {
+    const isDone = filterTodoList.filter((item) => item.isDone === true);
+    setIsDoneList(isDone.length);
+  }, [filterTodoList, todosList, choosedDate]);
+
   return (
     <>
       <HeaderText>{clickedDate}</HeaderText>
+
+      {filterTodoList.length > 0 && (
+        <span>
+          {isDoneList}/{filterTodoList.length}
+        </span>
+      )}
+
       <DividedLine />
     </>
   );
@@ -16,6 +44,7 @@ export default function TodosHeader({ clickedDate }) {
 
 const HeaderText = styled.p`
   ${({ theme }) => theme.fonts.Body1};
+  margin-bottom: -2rem;
 `;
 
 const DividedLine = styled.hr`
