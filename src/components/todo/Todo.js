8da58e96ba1078b093766ba7d0a553e6329e6986
heewon lee <84234411+hiwon-lee@users.noreply.github.com>
2024-09-20 02:39:45 +0900

    feat : Add progress bar

--- src/components/todo/Todo.js
+++ src/components/todo/Todo.js
@@ -1,21 +1,55 @@
 import { useLocalStorage } from '../../hooks/useLocalStorage';
-import { useEffect } from 'react';
+import { useEffect, useMemo } from 'react';
+import styled from 'styled-components';
 import TodoForm from './TodoForm';
 import TodoList from './TodoList';
 
 export default function Todo() {
   const [todos, setTodos] = useLocalStorage('todos', []);
 
+  // 완료된 할 일의 개수
+  const completedCount = useMemo(
+    () => todos.filter((todo) => todo.isCompleted).length,
+    [todos]
+  );
+
+  // 진행률 계산 (완료된 할 일의 수 / 전체 할 일 수)
+  const progress = todos.length > 0 ? (completedCount / todos.length) * 100 : 0;
+
   useEffect(() => {
     console.log(todos);
-  }, [todos]);
+  });
   return (
-    <div>
+    <>
       <TodoForm setTodos={setTodos} />
-      <TodoList
-        todos={todos}
-        setTodos={setTodos}
-      />
-    </div>
+      <div>
+        <label htmlFor="todoProgress">진행률: {Math.round(progress)}%</label>
+        <progress
+          id="todoProgress"
+          max={todos.length} // 전체 할 일 수
+          value={completedCount} // 완료된 할 일 수
+        >
+          {Math.round(progress)}%
+        </progress>
+      </div>
+
+      <TodoContainer>
+        <TodoList
+          title="TODO"
+          todos={todos.filter((todo) => !todo.isCompleted)}
+          setTodos={setTodos}
+        />
+        <TodoList
+          title="완료"
+          todos={todos.filter((todo) => todo.isCompleted)}
+          setTodos={setTodos}
+        />
+      </TodoContainer>
+    </>
   );
 }
+
+const TodoContainer = styled.div`
+  display: flex;
+  gap: 1rem;
+`;
