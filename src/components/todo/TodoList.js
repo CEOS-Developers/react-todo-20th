309e75e7a0572003dadd115000249055c5d9a095
heewon lee <84234411+hiwon-lee@users.noreply.github.com>
2024-09-20 02:40:42 +0900

    feat : Add toggleComplete 함수

--- src/components/todo/TodoList.js
+++ src/components/todo/TodoList.js
@@ -1,23 +1,41 @@
+import { useCallback, useMemo } from 'react';
 import { TodoItem } from './TodoItem';
 
-export default function TodoList({ todos, setTodos }) {
+export default function TodoList({ title, todos, setTodos }) {
+  // 투두 토글 함수 :
+  const toggleComplete = useCallback(
+    (id) => {
+      setTodos((prevTodos) =>
+        prevTodos.map((todo) =>
+          todo.id === id ? { ...todo, isCompleted: !todo.isCompleted } : todo
+        )
+      );
+    },
+    [setTodos]
+  );
+
   // 투두 수정
   const handleEdit = (id) => {
     console.log(`Edit todo with id: ${id}`);
   };
 
   // 투두 삭제
-  const handleDelete = (id) => {
-    setTodos(todos.filter((todo) => todo.id !== id));
-  };
+  const handleDelete = useCallback(
+    (id) => {
+      setTodos((prevTodos) => prevTodos.filter((todo) => todo.id !== id));
+    },
+    [setTodos]
+  );
   return (
     <div>
+      <div>{title}</div>
       <ul>
         {todos.map((todo) => (
           <TodoItem
             {...todo}
             onEdit={() => handleEdit(todo.id)}
             onDelete={() => handleDelete(todo.id)}
+            toggleComplete={() => toggleComplete(todo.id)}
           />
         ))}
       </ul>
