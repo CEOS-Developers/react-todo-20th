cbf5d174952c3f455467669b8f546a83c3911002
heewon lee <84234411+hiwon-lee@users.noreply.github.com>
2024-09-20 15:46:29 +0900

    chore : 날짜 추가

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -1,12 +1,46 @@
 import { useState, useEffect } from 'react';
+import styled from 'styled-components';
 
+const days = [
+  '일요일',
+  '월요일',
+  '화요일',
+  '수요일',
+  '목요일',
+  '금요일',
+  '토요일',
+];
 export default function Clock() {
   const [time, setTime] = useState(() => new Date());
+
+  // 날짜 세팅하기
+  const date = new Date();
+  const month = date.getMonth() + 1;
+  const day = date.getDate();
+  const getDay = days[date.getDay()];
+
   useEffect(() => {
     const id = setInterval(() => {
       setTime(new Date());
     }, 1000);
     return () => clearInterval(id);
   }, []);
-  return <h1>{time.toLocaleTimeString()}</h1>;
+
+  return (
+    <StyledClock>
+      <p className="date">
+        {month}월 {day}일 {getDay}
+      </p>
+      <p>{time.toLocaleTimeString()}</p>
+    </StyledClock>
+  );
 }
+
+const StyledClock = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+
+  font-size: 0.7rem;
+  color: rgb(220, 220, 220);
+`;
