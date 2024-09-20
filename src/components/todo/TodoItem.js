c9c04c5e52ab674dfaee29dcf68ba70806f00391
heewon lee <84234411+hiwon-lee@users.noreply.github.com>
2024-09-20 15:40:14 +0900

    chore : edit 기능 삭제

--- src/components/todo/TodoItem.js
+++ src/components/todo/TodoItem.js
@@ -2,41 +2,36 @@ import { IconButton, BlueButton, IsCompletedButton } from '../Button';
 import styled from 'styled-components';
 
 import { FaTrash } from 'react-icons/fa';
-import { MdEdit } from 'react-icons/md';
-
+import { HiCheck } from 'react-icons/hi';
+import { RiCheckboxBlankCircleFill } from 'react-icons/ri';
+import { IoCheckmarkCircle } from 'react-icons/io5';
 export function TodoItem({
   id,
   value,
   isCompleted = false,
-  onEdit,
+
   onDelete,
   toggleComplete,
 }) {
   return (
     <StyledItem id={id}>
-      <div className="todo">
-        {isCompleted ? (
-          <IsCompletedButton
-            type="button"
-            onClick={toggleComplete} // 완료로 상태 변경
-            $isCompleted="true"
-          ></IsCompletedButton>
-        ) : (
-          <IsCompletedButton
-            type="button"
-            onClick={toggleComplete} // 완료 취소
-            $isCompleted="false"
-          ></IsCompletedButton>
-        )}
-        <span>{value}</span>
-      </div>
-      <div className="button-group">
-        <IconButton
+      {isCompleted ? (
+        <IsCompletedButton
           type="button"
-          onClick={onEdit}
+          onClick={toggleComplete} // 완료로 상태 변경
+          $isCompleted="true"
         >
-          <MdEdit />
-        </IconButton>
+          <IoCheckmarkCircle />
+        </IsCompletedButton>
+      ) : (
+        <IsCompletedButton
+          type="button"
+          onClick={toggleComplete} // 완료 취소
+          $isCompleted="false"
+        ></IsCompletedButton>
+      )}
+      <div className="todo">{value}</div>
+      <div className="button-group">
         <BlueButton
           type="button"
           onClick={onDelete}
@@ -51,7 +46,8 @@ export function TodoItem({
 const StyledItem = styled.li`
   margin-top: 12px;
   display: flex;
-  flex-direction: row;
+  // min-width: 0; /* 긴 텍스트로 인해 박스가 커지는 문제를 방지 */
+  // word-wrap: break-word; /* 텍스트가 넘치지 않도록 줄바꿈 */
   align-items: center;
   font-size: 14px;
   padding: 1rem;
@@ -60,11 +56,9 @@ const StyledItem = styled.li`
   justify-content: space-between;
 
   .todo {
-    display: flex;
-    gap: 0.5rem;
     margin-right: auto;
+    justify-content: center;
     span {
-      // padding: 1rem;
       margin: 1rem;
     }
   }
