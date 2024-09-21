aaa2602577f1e53730fa6a1aa155218e40f33d56
heewon lee <84234411+hiwon-lee@users.noreply.github.com>
2024-09-21 22:50:49 +0900

    chore : ul 스타일코드 위치변경

--- src/components/todo/TodoForm.js
+++ src/components/todo/TodoForm.js
@@ -1,11 +1,13 @@
+import styled from 'styled-components';
 import { Button } from '../Button';
 import { useState } from 'react';
 
+// 할 일 입력 폼
 export default function TodoForm({ setTodos }) {
   const [inputValue, setInputValue] = useState('');
 
   const handleSubmit = (e) => {
-    e.preventDefault(); // form 제출 시 페이지 리로드 방지
+    e.preventDefault();
     console.log(inputValue);
     if (inputValue.trim()) {
       setTodos((prevTodos) => [
@@ -21,11 +23,11 @@ export default function TodoForm({ setTodos }) {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
+    <StyledForm onSubmit={handleSubmit}>
       <input
         type="text"
         placeholder="할 일 추가"
-        value={inputValue} // inputValue 상태값을 value 속성에 연결
+        value={inputValue} // inputValue 상태값을 value 속성에 연결 : 값 가져오려면 필요함
         onChange={(e) => setInputValue(e.target.value)}
       />
       <Button
@@ -34,6 +36,34 @@ export default function TodoForm({ setTodos }) {
       >
         추가
       </Button>
-    </form>
+    </StyledForm>
   );
 }
+
+const StyledForm = styled.form`
+  border: 0.5rem 1rem;
+  margin-top: 2rem;
+  display: flex;
+  flex-wrap: wrap;
+  font-size: 14px;
+  min-height: 48px;
+  min-width: 320px;
+  padding: 0.5rem 1rem;
+  gap: 8px;
+  border-radius: 12px;
+  color: var(--main-color);
+  background-color: var(--bg-primary);
+
+  input {
+    flex-grow: 1;
+    color: white;
+    margin-right: auto;
+    background-color: var(--bg-primary);
+    border: none;
+    outline: none;
+    font-size: 16px;
+  }
+  input::placeholder {
+    color: var(--main-color);
+  }
+`;
